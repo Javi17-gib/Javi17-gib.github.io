2bd4d712347a02a51e19869a426750da885306fb
Javi17-gib <luisvalverde1330@gmail.com>
2024-10-22 12:16:11 -0600

    js

--- E-COMMERCE/com.js
+++ E-COMMERCE/com.js
@@ -9,10 +9,3 @@ darkmode.onclick = () => {
         document.body.classList.remove('color');
     }
 }
-const sr = ScrollReveal({
-    distance: '10px',
-    duration: '2400',
-    rest: true
-});
-sr.reveal('.contenido-texto',{delay:250, origin: 'top'})
-sr.reveal('.contenido-img',{delay:550, origin: 'botton'})
