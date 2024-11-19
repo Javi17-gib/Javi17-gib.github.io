5c8c191d606217a5b1871a1ebc2e21e7d146aea2
Javi17-gib <luisvalverde1330@gmail.com>
2024-11-19 10:36:19 -0600

    hoy

---
+++ E-COMMERCE/JS/save.js
@@ -0,0 +1,11 @@
+window.addEventListener("load", function(){
+    document.getElementById("btnsave").addEventListener("click",function(){
+        alert("Productos Guardados Exitosamente")
+    })
+})
+
+window.addEventListener("load", function(){
+    document.getElementById("btnAgregarr").addEventListener("click",function(){
+        alert("Favor de llenar los campos")
+    })
+})
